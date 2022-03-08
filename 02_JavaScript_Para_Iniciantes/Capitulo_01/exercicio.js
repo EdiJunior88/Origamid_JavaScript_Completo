e5b8d6c3bb71818c58f71db8e0dade89292698ae
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-08 17:31:48 -0300

    Variáveis

---
+++ 02_JavaScript_Para_Iniciantes/Capitulo_01/exercicio.js
@@ -0,0 +1,26 @@
+// Declarar uma variável com o seu nome
+
+var nome = "Edivaldo";
+
+// Declarar uma variável com a sua idade
+
+var idade = 34;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var pizzaComMussarela;
+
+// Atribuir valor a sua comida favorita
+
+pizzaComMussarela = true;
+
+// Declarar 5 variáveis diferentes sem valores
+
+var time,
+    estadio,
+    estado,
+    confederacao,
+    clube;
+
+console.log(nome, idade, pizzaComMussarela);
---
+++ 02_JavaScript_Para_Iniciantes/Capitulo_01/index.js
@@ -0,0 +1,34 @@
+var nome = "André";
+var idade = 23;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, idade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Rafael",
+    cidade = "Rio de Janeiro";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+
+console.log(semDefinir);
+
+//console.log(aindaNaoDefine);
+
+console.log(comida);
+
+var comida;
+console.log(comida);
+var comida = "Pizza";
+console.log(comida);
+
+// const time = "Vasco";
+// time = "Flamengo";
+
+// console.log(time);
