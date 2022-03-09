97e5a6bd4abfc6efe3181165b48929ee9ddf77dc
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-08 21:04:24 -0300

    Tipos de Dados

---
+++ 02_JavaScript_Para_Iniciantes/Capitulo_02/exercicio.js
@@ -0,0 +1,28 @@
+// Declare uma variável contendo uma string
+
+var nome = "Edivaldo";
+console.log(typeof nome);
+
+// Declare uma variável contendo um número dentro de uma string
+
+var numeral = "1024";
+
+// Declare uma variável com a sua idade
+
+var idade = 34;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+var nome = "Edivaldo";
+var sobrenome = "Junior"
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var frase = `It's time`;
+
+// Verifique o tipo da variável que contém o seu nome
+
+var verificarTipoNome = typeof nome;
+console.log(verificarTipoNome);
