98f796224468d61a7ee3629dea47bfacb6da7ecd
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-09 10:38:27 -0300

    Números e Operadores 2

---
+++ 02_JavaScript_Para_Iniciantes/Capitulo_04/exercicio.js
@@ -0,0 +1,23 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; //35
+
+// Crie duas expressões que retornem NaN
+var soma1 = 'Testando valor';
+var soma2 = 50;
+console.log(soma1 / soma2);
+
+// Somar a string '200' com o número 50 e retornar 250
+var string = +'200';
+var numero = 50;
+console.log(string + numero); 
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var incremento = 5;
+console.log(++incremento);
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = numero / 2; // NaN (Not a Number)
+console.log(pesoPorDois + unidade);
