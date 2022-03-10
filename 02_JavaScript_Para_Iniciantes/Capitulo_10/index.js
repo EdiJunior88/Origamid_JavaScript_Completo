c9f5cdeba36480944d62d500422af1c4e48ca4f7
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-10 09:39:42 -0300

    Objetos 1

---
+++ 02_JavaScript_Para_Iniciantes/Capitulo_10/index.js
@@ -0,0 +1,21 @@
+var pessoa = {
+  nome: "André",
+  idade: 28
+}
+
+console.log(pessoa.idade);
+
+var quadrado = {
+  lados: 4,
+  area(lado) {
+    return lado * lado;
+  },
+  perimetro(lado) {
+    return this.lados * lado;
+  },
+}
+
+console.log(quadrado.area(5));
+console.log(quadrado.perimetro(5));
+
+console.log(Math.random());
