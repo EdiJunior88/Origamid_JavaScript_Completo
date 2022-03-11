99b013a7cb1dd3bed0104c776aad47ed19bb5467
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-11 19:38:30 -0300

    Arrays e Loops 1

---
+++ 02_JavaScript_Para_Iniciantes/Capitulo_13/index.js
@@ -0,0 +1,14 @@
+var videoGames = ["Switch", "PS4", "Xbox"];
+
+var ultimoItem = videoGames.pop();
+
+for (var numero = 1; numero <= 10; numero++) {
+  console.log(numero);  
+}
+
+var i = 0;
+while (i < 10) {
+  console.log(i);
+  i = i + 5;
+}
+// Retorna de 0 a 9 no console
