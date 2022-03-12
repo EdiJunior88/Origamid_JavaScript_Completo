437deb4cc9fcff89d86556416f84409c1fd21911
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-12 20:20:45 -0300

    O Que é o Dom 1

---
+++ 03_DOM_para_iniciantes/Capitulo_01/index.js
@@ -0,0 +1,13 @@
+window.alert("Isso mesmo");
+
+const href = window.location.href
+
+console.log(href);
+
+if (href === "http://127.0.0.1:3000/c:/Users/edijunior/Desktop/programacao/Origamid_JavaScript_Completo/03_DOM_para_iniciantes/Capitulo_01/index.html") {
+  console.log("É igual");
+}
+
+const h1Selecionado = document.querySelector("h1");
+
+alert("Teste")
