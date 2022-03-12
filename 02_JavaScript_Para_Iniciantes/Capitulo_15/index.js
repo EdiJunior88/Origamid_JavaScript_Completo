67e887108d92bf1034103e2ccf6810c7fcdddc54
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-11 21:49:59 -0300

    Atribuição e Ternário

---
+++ 02_JavaScript_Para_Iniciantes/Capitulo_15/index.js
@@ -0,0 +1,21 @@
+var numero = 20;
+var numero2 = 10;
+
+numero /= numero2; //numero = numero + 10
+console.log(numero);
+console.log(numero2);
+
+
+var idade = 20;
+naoPossuiDiabetes = false;
+
+var podeBeber;
+podeBeber = (idade >= 18 && naoPossuiDiabetes) ? "Pode beber" : "Não pode beber";
+
+console.log(podeBeber);
+
+var possuiFaculdade = true;
+if (possuiFaculdade)
+  console.log("Sim possui");
+else
+  console.log("Não possui");
