190e3f0d6c4aaba6f5d643984de5764f1bad32e3
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-14 19:44:15 -0300

    Seleção de Elementos 1

---
+++ 03_DOM_para_iniciantes/Capitulo_06/index.js
@@ -0,0 +1,17 @@
+const animais = document.getElementById("animais");
+console.log(animais);
+
+const gridSection = document.getElementsByClassName("grid-section");
+console.log(gridSection[1]);
+
+const primeiraLi = document.querySelector("li");
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector("ul");
+console.log(primeiraUl);
+
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno);
+
+const animaisImg = document.querySelectorAll(".animais img");
+console.log(animaisImg[0]);
